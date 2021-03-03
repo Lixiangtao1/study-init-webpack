2d2b32822a18ed79b68654463b7edb6696187508
李向涛 <1549854846@qq.com>
2021-03-03 15:46:28 +0800

    初始webpack

---
+++ 02webpack初体验/src/index.js
@@ -0,0 +1,20 @@
+/* 
+  index.js: webpack入口起点文件
+
+  1.运行指令: 
+    开发环境: webpack ./src/index.js -o ./build/built.js --mode=development
+      (webpack会以./src/index.js为入口文件开始打包, 打包后输出到./build/built.js)
+    生产环境: webpack ./src/index.js -o ./build/built.js --mode=production
+
+  2.结论：
+    a : webpack能够处理JS资源,JSON资源, 不能够处理CSS/img资源
+    b : 生产环境和开发环境将ES6模块化编译成浏览器能识别的模块化
+    c : 生产环境比开发环境多一个JS压缩代码
+*/ 
+// import './index.css'  //引入css资源时打包报错
+import data from './data.json'
+console.log(data)
+function test(a,b){
+  return a + b;
+}
+console.log(test(1,2))
